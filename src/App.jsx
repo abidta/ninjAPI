38918220198332316ec88e311315ec32fc0fc1f6
Sainul Abid <sainulabidofficial@gmail.com>
2023-11-07 23:34:51 +0530

    vite init
    remove unwanted codes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div></div>
+      <h1>ninjApi</h1>
+      <h3>coming soon...</h3>
+
+      <p className="read-the-docs">
+        <a href="https://github.com/abidta/ninjAPI">Github</a>
+      </p>
+    </>
+  );
+}
+
+export default App;
