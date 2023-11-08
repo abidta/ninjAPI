d54db988261dc82a646030fa3667f9cb0cdf542e
Sainul Abid <sainulabidofficial@gmail.com>
2023-11-08 19:03:19 +0530

    bootstrap config on vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path,{ dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  }
 })
