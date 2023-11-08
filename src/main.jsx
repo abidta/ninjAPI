6045d21bf5e7d55c31097aa86ad6e461da309294
Sainul Abid <sainulabidofficial@gmail.com>
2023-11-08 16:14:53 +0530

    Bootstrap config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './sass/styles.scss'
+// eslint-disable-next-line no-unused-vars
+//import * as bootstrap from 'bootstrap'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
