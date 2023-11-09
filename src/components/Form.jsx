e86c27ead41f44a3db03fcf6b87a214342e34363
Sainul Abid <sainulabidofficial@gmail.com>
2023-11-09 23:38:23 +0530

    axios setup

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 import TabContent from './TabContent'
@@ -7,6 +8,7 @@ import UrlMethods from './UrlMethods'
 function Form() {
   const [formData, setFormData] = useState({
     url: '',
+    method: '',
     params: [] || {},
     headers: [],
   })
@@ -19,8 +21,17 @@ function Form() {
   const handleUrls = (url) => {
     setFormData({ ...formData, url })
   }
-  const handleMethods =(method)=>{
+  const handleMethods = (method) => {
     setFormData({ ...formData, method })
+  }
+  const toObject = (array) => {
+    return array.reduce((data, pair) => {
+      console.log(data, 'klkl')
+      let key = pair.key
+      let value = pair.value
+      if (key === '') return data
+      return { ...data, [key]: value }
+    }, {})
   }
   return (
     <div className="p-4">
@@ -28,8 +39,14 @@ function Form() {
         action=""
         onSubmit={(e) => {
           e.preventDefault()
-
-          console.log('formk', formData)
+          axios({
+            url: formData.url,
+            method: formData.method,
+            params: toObject(formData.params),
+            headers: toObject(formData.headers),
+          }).then((response)=>{
+            console.log(response,'axios');
+          })
         }}
       >
         <UrlMethods setMethod={handleMethods} onChange={handleUrls} />
