50b0ae11dc86623ec7bd59a2057086c16f05606a
Sainul Abid <sainulabidofficial@gmail.com>
2023-11-13 16:36:24 +0530

    spinner changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Spinner from 'react-bootstrap/Spinner'
 import Form from './components/Form'
 import Response from './components/Response'
 import Error from './components/Error'
@@ -8,22 +7,24 @@ import Error from './components/Error'
 function App() {
   const [response, setResponse] = useState({})
   const [loader, setLoader] = useState(false)
+
   const responseHandler = (response) => {
     setLoader(false)
     setResponse(response)
   }
   const handleLoader = () => {
     setLoader(true)
+    setResponse({})
   }
   return (
     <>
       <Form onLoading={handleLoader} onResponse={responseHandler} />
       {loader && (
-        <div className="col-md-5 mx-auto text-center">
+        <div className="col-md-5 mt-5 mx-auto text-center">
           {' '}
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
+          <span className="loader"></span>
+          <br />
+          <span className='ms-1'>Loading...</span>
         </div>
       )}
       {response.message && <Error err={'something went wrong'} />}
